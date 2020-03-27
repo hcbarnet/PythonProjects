a51080224352cf170225100a445b9e1316c26a89
hcbarnet <hcbarnet@iu.edu>
2020-03-27 01:35:13 -0400

    Secure Pasword Maker Upgraded For Python 3+

--- SecurePasswordMaker/Secure_Password_Maker.py
+++ SecurePasswordMaker/Secure_Password_Maker.py
@@ -6,20 +6,15 @@
 #    Mar 26, 2020 11:34:56 PM EDT  platform: Linux
 
 import sys
-import Tkinter as tk
-from Tkinter import *
 import random
 import string
 
-    
 
 try:
     import ttk
-    from ttk import *
     py3 = False
 except ImportError:
     import tkinter.ttk as ttk
-    from ttk import *
     py3 = True
 
 import Secure_Password_Maker_Support
@@ -29,21 +24,25 @@ def vp_start_gui():
     '''Starting point when module is the main routine.'''
     global val, w, root
     root = tk.Tk()
-    top = Toplevel2 (root)
+    top = Toplevel2(root)
     Secure_Password_Maker_Support.init(root, top)
     root.mainloop()
 
+
 w = None
+
+
 def create_Toplevel2(rt, *args, **kwargs):
     '''Starting point when module is imported by another module.
        Correct form of call: 'create_Toplevel2(root, *args, **kwargs)' .'''
     global w, w_win, root
     #rt = root
     root = rt
-    w = tk.Toplevel (root)
-    top = Toplevel2 (w)
+    w = tk.Toplevel(root)
+    top = Toplevel2(w)
     Secure_Password_Maker_Support.init(w, top, *args, **kwargs)
     return (w, top)
+
 
 def destroy_Toplevel2():
     global w
@@ -53,16 +52,14 @@ def destroy_Toplevel2():
 
 class Toplevel2:
 
-    
-
     def __init__(self, top=None):
         '''This class configures and populates the toplevel window.
            top is the toplevel containing window.'''
         _bgcolor = '#d9d9d9'  # X11 color: 'gray85'
         _fgcolor = '#000000'  # X11 color: 'black'
-        _compcolor = '#d9d9d9' # X11 color: 'gray85'
-        _ana1color = '#d9d9d9' # X11 color: 'gray85'
-        _ana2color = '#ececec' # Closest X11 color: 'gray92'
+        _compcolor = '#d9d9d9'  # X11 color: 'gray85'
+        _ana1color = '#d9d9d9'  # X11 color: 'gray85'
+        _ana2color = '#ececec'  # Closest X11 color: 'gray92'
 
         top.geometry("600x300+409+129")
         top.minsize(599, 300)
@@ -76,7 +73,8 @@ class Toplevel2:
         self.Label1.configure(activebackground="#262626")
         self.Label1.configure(activeforeground="white")
         self.Label1.configure(background="#262626")
-        self.Label1.configure(font="-family {DejaVu Sans Mono} -size 20 -weight bold")
+        self.Label1.configure(
+            font="-family {DejaVu Sans Mono} -size 20 -weight bold")
         self.Label1.configure(foreground="#0c912f")
         self.Label1.configure(text='''Secure Password Generator''')
 
@@ -84,44 +82,32 @@ class Toplevel2:
         self.Button1.place(relx=0.25, rely=0.733, height=61, width=300)
         self.Button1.configure(activebackground="#f9f9f9")
         self.Button1.configure(background="#ffffff")
-        self.Button1.configure(font="-family {DejaVu Sans Mono} -size 14 -weight bold")
+        self.Button1.configure(
+            font="-family {DejaVu Sans Mono} -size 14 -weight bold")
         self.Button1.configure(foreground="#0c912f")
         self.Button1.configure(text='''Generate''')
-	self.Button1.configure(command = self.generate_password)
+        self.Button1.configure(command=self.generate_password)
 
         self.Entry1 = tk.Entry(top)
-        self.Entry1.place(relx=0.125, rely=0.4,height=33, relwidth=0.75)
+        self.Entry1.place(relx=0.125, rely=0.4, height=33, relwidth=0.75)
         self.Entry1.configure(background="white")
-        self.Entry1.configure(font=("TkFixedFont",7))
+        self.Entry1.configure(font=("TkFixedFont", 7))
         self.Entry1.configure(selectbackground="#c4c4c4")
-	self.Entry1_var = StringVar()
-	self.Entry1_var.set('Generated Password Here')
-	self.Entry1.configure(textvariable=self.Entry1_var)
-	
-
-	
-	
+        self.Entry1_var = StringVar()
+        self.Entry1_var.set('Generated Password Here')
+        self.Entry1.configure(textvariable=self.Entry1_var)
 
     def generate_password(self):
-	##Nist Password Guidlines 
-	##Length = 8 - 64 characters
-	##All characters including a space
-	password_length = random.randint(7,65)
-	password = ""
-	for i in range(password_length):
-		character = string.printable[random.randint(0, 65)]
-		password = password + character
-		self.Entry1_var.set(password)
-		
-		
-        
-
+        # Nist Password Guidlines
+        # Length = 8 - 64 characters
+        # All characters including a space
+        password_length = random.randint(7, 65)
+        password = ""
+        for i in range(password_length):
+            character = string.printable[random.randint(0, 65)]
+            password = password + character
+            self.Entry1_var.set(password)
 
 
 if __name__ == '__main__':
     vp_start_gui()
-
-
-
-
-
